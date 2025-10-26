941bc4954f76902b7845a19d4504f3cbe98d9810
Utkarsh <onlyutkarshjoshilkar@gmail.com>
2025-10-26 10:40:33 +0530

    bug fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -181,7 +181,7 @@ const Contact = () => {
                   </div>
                   <div className="contact-details">
                     <h4>Location</h4>
-                    <p>Mahagaon, Mahagrastra </p>
+                    <p>Mahagaon, Maharashtra </p>
                   </div>
                 </div>
               </div>
